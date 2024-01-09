cf9589ce32de6abea898734895d815f75e858642
Athens410 <athenah.coucean@gmail.com>
2024-01-09 16:53:12 -0500

    added console log
    test

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 let apiKey = "06a4f404921b3294b64d64f35o0f753t";
 
 let context = "be polite and provide a very short answer";
+
+let prompt = "when was the first ai ever created";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+console.log(apiUrl);
