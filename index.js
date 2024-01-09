4d214e8ba3f247fdf3ad1429e16efb49c6ea5f1c
Athens410 <athenah.coucean@gmail.com>
2024-01-09 17:30:15 -0500

    asking different prompts

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let apiKey = "06a4f404921b3294b64d64f35o0f753t";
 
 let context = "be polite and provide a very short answer";
 
-let prompt = "how many pizza topping are there";
+let prompt = "do penguins fly";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 
