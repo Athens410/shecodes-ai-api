7220408ea33efdc01c5c18fdd7e6c7ed4ab02b48
Athens410 <athenah.coucean@gmail.com>
2024-01-09 16:48:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let apiKey = "06a4f404921b3294b64d64f35o0f753t";
 
 
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=&context={context}&key={key}`;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key={key}`;
