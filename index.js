b722cb0832aff2bb3007c7445153a6a5f0eda777
Athens410 <athenah.coucean@gmail.com>
2024-01-09 16:50:51 -0500

    added another context variable

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let apiKey = "06a4f404921b3294b64d64f35o0f753t";
 
-
+let contex = "be polite and provide a very short answer";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
