8d62c1faeac8ded056371d78df2194d375753c82
Athens410 <athenah.coucean@gmail.com>
2024-01-09 17:05:44 -0500

    added showAnswer function
    trying console log again

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+
+function showAnswer(response){
+
+    console.log(response);
+}
+
+
+
+
+
 let apiKey = "06a4f404921b3294b64d64f35o0f753t";
 
 let context = "be polite and provide a very short answer";
@@ -6,4 +16,4 @@ let prompt = "when was the first ai ever created";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 
-console.log(apiUrl);
+axios.get(apiUrl).then(showAnswer);
