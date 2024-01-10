3bc0ea26473cf9889728adecc69e4c3197fee2b9
Athens410 <athenah.coucean@gmail.com>
2024-01-10 18:44:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
 
-function showAnswer(response){
-
-    alert(response.data.answer);
-}
-
-
-
-
-
-let apiKey = "06a4f404921b3294b64d64f35o0f753t";
-
-let context = "be polite and provide a very short answer";
-
-let prompt = "who was the first female president";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-
-axios.get(apiUrl).then(showAnswer);
-
-
-console.log("Processing");
 
 let buttonElement = document.querySelector("#special-button");
 
@@ -28,7 +7,7 @@ function handleClick(){
 
 
 
-    
+
 }
 
 
