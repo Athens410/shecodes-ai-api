ffb6a6b988ec9e9900210142520f65f55218002f
Athens410 <athenah.coucean@gmail.com>
2024-01-10 19:17:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,6 @@ let buttonElement = document.querySelector("#special-button");
 buttonElement.addEventListener("click", handleClick);
 
 buttonElement.style.background = "blue";
+
+
+let heading = document.querySelector("h1")
