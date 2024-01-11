558218439e3073921926858d6d3a8d053130f6b2
Athens410 <athenah.coucean@gmail.com>
2024-01-11 11:04:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 
 
+let heading = document.querySelector("h1");
+
+
+
+let buttonElement = document.querySelector("#special-button");
+buttonElement.addEventListener("click", handleClick);
+
+
+
+
+
 
