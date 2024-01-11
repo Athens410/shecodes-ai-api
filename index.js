69b25a7f9175a35346e543dbfe91e9a93746635e
Athens410 <athenah.coucean@gmail.com>
2024-01-11 12:09:02 -0500

    making a joke generator

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 
 
 
+let button = document.querySelector("#special-button");
+
+button.addEventListener("click")
+
+
+
 new Typewriter("h1", {
   strings: "Hello World eat carrots",
   autoStart: true,
