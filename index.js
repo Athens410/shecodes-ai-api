b3c7da673bc1fe9f8aeeaed0be15a0efd438b89a
Athens410 <athenah.coucean@gmail.com>
2024-01-10 19:04:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,3 @@ alert("hey")
 let buttonElement = document.querySelector("#special-button");
 buttonElement.addEventListener("click", handleClick);
 
-buttonElement.setAttribute("id", "gopop");
-
-
-console.log("Script loaded"); 
