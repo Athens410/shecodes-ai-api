9e06d385cbbab61981cc239495fae8338487c4f0
Athens410 <athenah.coucean@gmail.com>
2024-01-11 11:06:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ let heading = document.querySelector("h1");
 
 
 
-let buttonElement = document.querySelector("#special-button");
-buttonElement.addEventListener("click", handleClick);
-
-
-
+h1.innerHTML = "hello world"
 
 
 
