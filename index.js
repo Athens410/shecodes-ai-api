28650f6b998d945d98660ca160b4ef9cc412caaa
Athens410 <athenah.coucean@gmail.com>
2024-01-11 12:12:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function newWorld(response){
 
 let button = document.querySelector("#special-button");
 let apiKey = "06a4f404921b3294b64d64f35o0f753t";
+let apiUrl = "https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}";
 let context =  "please provide a simple answer";
 
 let prompt = "who is the first female president";
